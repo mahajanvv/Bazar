6ff222cf8970661778b6bb69d441f4d1617d36c8
Vinit Vijaykumar Mahajan <vinitmahajan1@gmail.com>
2018-03-21 07:24:03 +0530

    mahavin

--- loignserver.php
+++ loignserver.php
@@ -1,42 +1,4 @@
 <?php
-//$error = '';
-//if(isset($_POST["submit"]))
-//{
-//	//$error = 'failed';
-//	if(empty($_POST['uname']) || empty($_POST['upassword']))
-//	{
-//		$error = 'UserName OR PassWord is Wrong';
-//		
-//	}
-//	else
-//	{
-//		$user = $_POST['uname'];
-//		$password = $_POST['upassword'];
-//		
-//		$conn = mysqli_connect("localhost","root","");
-//		
-//		$db = mysqli_select_db($conn,"Bazar");
-//		
-//		
-//		
-//		$rows = mysqli_num_rows($query);
-//		if($rows == 1)
-//		{
-//			header("Location::welcome.php");
-//			
-//		}
-//		else
-//		{
-//			$error = 'Invalid username or password re-enter';
-//		}
-//		mysqli_close($conn);
-//	}
-//	
-//}
-//else
-//{
-//	$error = 'failed';
-//}
 
         $user = $_POST['uname'];
 		$password = $_POST['upassword'];
