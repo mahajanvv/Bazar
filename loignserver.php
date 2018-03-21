ba0d07da13a6312409f75f4ffb7d6f3eb43f1163
Vinit Vijaykumar Mahajan <vinitmahajan1@gmail.com>
2018-03-21 07:38:03 +0530

    am also done

--- loignserver.php
+++ loignserver.php
@@ -1,12 +1,5 @@
 <?php
 
-//		$query = mysqli_query($conn,"SELECT * FROM login WHERE uname LIKE '".$user."' and upassword LIKE '".$password."'");
-        $query = mysqli_query($conn,"SELECT * FROM login WHERE uname = '$user' and upassword = '$password'");
-        if(!$conn){
-            echo "Error is ".mysqli_error($conn);
-        }
-		$result=mysqli_fetch_all($query,MYSQLI_ASSOC);
-        var_dump($result);
 //		$rows = mysqli_num_rows($query);
         $cnt=count($result);
 		if($cnt == 1)
